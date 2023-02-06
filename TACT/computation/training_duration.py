a0d0e495d168be00a1256302a15bcb89635cf641
Alexandra E. Arntsen <AEA@nrgsystems.com>
2023-02-06 08:26:45 -0500

    add training_duration module

---
+++ TACT/computation/training_duration.py
@@ -0,0 +1,59 @@
+import numpy as np
+
+
+def test_train_split_convergence(x, y):
+    """incrementally increase the % parameter for test train split to check for convergence
+
+    Parameters
+    ----------
+    x : type
+        description
+    y : type
+        description
+
+    Returns
+    -------
+    pandas datafram
+
+    """
+    x_new = x
+    return x_new
+
+
+def test_tain_split_add_days(x,y):
+    """incrementally add days to test train split
+
+    Parameters
+    ----------
+    x : float
+        description
+    y : float
+        description
+    
+
+    Returns
+    -------
+    pandas dataframe
+        
+    """
+    x_new = x
+    return x_new
+
+def test_tain_split_sliding_window(x,y):
+    """if data is greater than 3 months, use a sliding window for test train split
+
+    Parameters
+    ----------
+    x : float
+        description
+    y : float
+        description
+    
+
+    Returns
+    -------
+    pandas dataframe
+        
+    """
+    x_new = x
+    return x_new
